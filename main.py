55bde702d33010ad5334fc315a48eeb3d16ffa35
Luke-Trivette <157540127+Luke-Trivette@users.noreply.github.com>
2024-02-01 15:33:09 -0600

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
-
+    if 0 < index < len(word):
+        char_at_index = word[index]
+    if char_at_index.isalpha():
+        return "letter"
+    elif char_at_index.isdigit():
+      return "number"
+    elif char_at_index.isspace():
+      return "whitespace"
+    else:
+      return "unknown"
+    
+    
+   
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
     print(check_character('happy birthday', 5))
